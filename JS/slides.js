1bdc05d5429a28ff935be856ecadea21ce66dd50
Lucas Bonato <lucas.perez.bonato@gmail.com>
2023-06-07 21:20:10 -0300

    new slider

--- JS/slides.js
+++ JS/slides.js
@@ -1,8 +1,74 @@
-let slides = document.getElementsByClassName('splide');
+const slider = document.querySelector('.slider-container'),
+  slides = Array.from(document.querySelectorAll('.slide'))
 
-for(let i = 0; i < slides.length; i++){
-    new Splide(slides[i], {
-        type   : 'loop',
-        autoplay: true,
-    }).mount();
+// set up our state
+let isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID,
+  currentIndex = 0
+
+// add our event listeners
+slides.forEach((slide, index) => {
+  const slideImage = slide.querySelector('img')
+  // disable default image drag
+  slideImage.addEventListener('dragstart', (e) => e.preventDefault())
+  // pointer events
+  slide.addEventListener('pointerdown', pointerDown(index))
+  slide.addEventListener('pointerup', pointerUp)
+  slide.addEventListener('pointerleave', pointerUp)
+  slide.addEventListener('pointermove', pointerMove)
+})
+
+// make responsive to viewport changes
+window.addEventListener('resize', setPositionByIndex)
+
+// use a HOF so we have index in a closure
+function pointerDown(index) {
+  return function (event) {
+    currentIndex = index
+    startPos = event.clientX
+    isDragging = true
+    animationID = requestAnimationFrame(animation)
+    slider.classList.add('grabbing')
+  }
+}
+
+function pointerMove(event) {
+  if (isDragging) {
+    const currentPosition = event.clientX
+    currentTranslate = prevTranslate + currentPosition - startPos
+  }
+}
+
+function pointerUp() {
+  cancelAnimationFrame(animationID)
+  isDragging = false
+  const movedBy = currentTranslate - prevTranslate
+
+  // if moved enough negative then snap to next slide if there is one
+  if (movedBy < -100 && currentIndex < slides.length - 1) currentIndex += 1
+
+  // if moved enough positive then snap to previous slide if there is one
+  if (movedBy > 100 && currentIndex > 0) currentIndex -= 1
+
+  setPositionByIndex()
+
+  slider.classList.remove('grabbing')
+}
+
+function animation() {
+  setSliderPosition()
+  if (isDragging) requestAnimationFrame(animation)
+}
+
+function setPositionByIndex() {
+  currentTranslate = currentIndex * -window.innerWidth
+  prevTranslate = currentTranslate
+  setSliderPosition()
+}
+
+function setSliderPosition() {
+  slider.style.transform = `translateX(${currentTranslate}px)`
 }
